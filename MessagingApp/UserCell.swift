aee9e6a4da2684c646e92228e34a44e07e97bac3
JamesSteiner <jamessteiner2002@outlook.com>
2017-08-31 16:29:56 +0100

    Messaging Application

---
+++ MessagingApp/UserCell.swift
@@ -0,0 +1,107 @@
+//
+//  UserCell.swift
+//  gameofchats
+//
+//  Created by James Steiner on 12/08/17.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+//Used in MessagesController for text views
+class UserCell: UITableViewCell {
+    
+    var message: Message? {
+        didSet {
+            setupNameAndProfileImage()
+            
+            //sets subtext to the most recent message
+            detailTextLabel?.text = message?.text
+            
+            if let seconds = message?.timestamp?.doubleValue {
+                let timestampDate = Date(timeIntervalSince1970: seconds)
+                
+                //formats the date correctly and sets writes in on the time label
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "hh:mm a"
+                timeLabel.text = dateFormatter.string(from: timestampDate)
+            }
+            
+            
+        }
+    }
+    
+    fileprivate func setupNameAndProfileImage() {
+        
+        if let id = message?.chatPartnerId() {
+            let ref = Database.database().reference().child("users").child(id)
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    //sets the text label to the chat partner's name
+                    self.textLabel?.text = dictionary["name"] as? String
+                    //for later implimentation
+//                    if let profileImageUrl = dictionary["profileImageUrl"] as? String {
+//                        self.profileImageView.loadImageUsingCacheWithUrlString(profileImageUrl)
+//                    }
+                }
+                
+                }, withCancel: nil)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //sets textLabel frame size
+        textLabel?.frame = CGRect(x: 64, y: textLabel!.frame.origin.y - 2, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        
+        //sets subtext frame size
+        detailTextLabel?.frame = CGRect(x: 64, y: detailTextLabel!.frame.origin.y + 2, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    //for later implimentation
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    //sytles for timeLabel
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor.darkGray
+        label.translatesAutoresizingMaskIntoConstraints = false //so we can edit constraints...
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView) //for later implimentation...
+        addSubview(timeLabel)
+        
+        //for later implimentation
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        //need x,y,width,height anchors
+        timeLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 18).isActive = true
+        timeLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        timeLabel.heightAnchor.constraint(equalTo: (textLabel?.heightAnchor)!).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
